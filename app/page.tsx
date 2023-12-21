569e5e6d9f5802f43979161cb978820ce13687ad
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2023-12-22 00:26:37 +0100

    Basic tokenami setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
 const Home = () => {
-  return <h1>Hello world</h1>;
+  return (
+    <h1
+      tabIndex={0}
+      style={{
+        "--padding": 10,
+        "--color": "var(--color-slate-700)",
+        "--hover_color": "var(--color-slate-100)",
+      }}
+    >
+      Hello world
+    </h1>
+  );
 };
 
 export default Home;
